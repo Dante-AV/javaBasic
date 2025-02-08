9b3a11f55ba5e80a744ce38acf1f5a3524711c5a
Anna <mysterious_16@mail.ru>
2025-02-08 18:05:23 +0300

    hw04

--- src/main/java/ru/otus/homework/hw04/Box.java
+++ src/main/java/ru/otus/homework/hw04/Box.java
@@ -6,7 +6,7 @@ public class Box {
     private final int height;
     public String color;
     private String item;
-    private boolean isOpenBox;
+    private boolean isOpen;
 
     public Box(int length, int width, int height, String color) {
         this.length = length;
@@ -15,48 +15,43 @@ public class Box {
         this.color = color;
     }
 
-
-    public void setColor(String color) {
-        this.color = color;
-    }
-
     public void setItem(String item) {
         this.item = item;
     }
 
     public void changeColor(String color) {
-        setColor(color);
+        this.color = color;
         System.out.println("Цвет коробки изменен на " + color);
     }
 
     public void openBox() {
-        this.isOpenBox = true;
+        this.isOpen = true;
         System.out.println("Коробку открыли");
     }
 
     public void closeBox() {
-        this.isOpenBox = false;
+        this.isOpen = false;
         System.out.println("Коробку закрыли");
     }
 
     public void printBoxInfo() {
-        System.out.println("Длина коробки: " + length + " Ширина коробки: " + width + " Высота коробки: " + height + " Цвет коробки: " + color + (isOpenBox ? ". Коробка открыта" : ". Коробка закрыта") + (this.item != null ? ". В коробке лежит " + this.item : ". В коробке нет предмета") + "\n");
+        System.out.println("Длина коробки: " + length + " Ширина коробки: " + width + " Высота коробки: " + height + " Цвет коробки: " + color + (isOpen ? ". Коробка открыта" : ". Коробка закрыта") + (this.item != null ? ". В коробке лежит " + this.item : ". В коробке нет предмета") + "\n");
     }
 
     public void putItem(String item) {
-        if (isOpenBox && item == null) {
+        if (isOpen && item == null) {
             setItem(item);
         } else {
-            System.out.println(isOpenBox ? "В коробку нельзя положить новый предмет " + item + ".В коробке уже лежит предмет - " + this.item : "В коробку нельзя положить предмет " + item + ", коробка закрыта ");
+            System.out.println(isOpen ? "В коробку нельзя положить новый предмет " + item + ".В коробке уже лежит предмет - " + this.item : "В коробку нельзя положить предмет " + item + ", коробка закрыта ");
         }
     }
 
     public void throwOutItem() {
-        if (isOpenBox && item != null) {
+        if (isOpen && item != null) {
             System.out.println("Из коробки выкинули предмет " + item);
             item = null;
         } else {
-            System.out.println(isOpenBox ? "Из коробки нельзя выкинуть предмет, потому что в коробке нет ни одного предмета" : "Из коробки нельзя выкинуть " + item + ", потому что коробка закрыта");
+            System.out.println(isOpen ? "Из коробки нельзя выкинуть предмет, потому что в коробке нет ни одного предмета" : "Из коробки нельзя выкинуть " + item + ", потому что коробка закрыта");
         }
     }
 }
